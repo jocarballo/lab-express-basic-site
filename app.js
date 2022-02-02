8759bb1aeb23d082b827a199f740602cd2edf632
Joana Carballo <joana16cruz@gmail.com>
2022-02-02 20:23:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+const app = express()
+const port = 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+	console.log('home route')
+	console.log(__dirname)
+	res.sendFile(__dirname + '/views/home-page.html')
+});
+
+app.get('/about', (req, res, next) => {
+	res.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('/work', (req, res, next) => {
+	res.sendFile(__dirname + '/views/work.html')
+});
+
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+});
---
+++ public/javascript/script.js
@@ -0,0 +1,3 @@
+document.querySelector('button').addEventListener('click', () => {
+	console.log('the button was clicked')
+})
